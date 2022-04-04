86ad18bc28dd4281ead71a12b7dbcd6b4e13427a
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-04-04 11:49:28 +0600

    header done

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Home.css'
+ 
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className='details-container'>
+                <h1>Eid Collection</h1>
+                <h1> Outstanding! Panjabi Review </h1>
+                <p>
+                    during eid, The first choice of dress is panjabi.At a time it is looking nice everyone.Anyone can give me a panjabi in eid time,i never mind.At least give you a lot of thank you.
+                </p>
+            </div>
+            <div className='photo-container'>
+                
+            </div>
+
+        </div>
+    );
+};
+
+export default Home;
