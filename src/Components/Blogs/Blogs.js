ad937f59c4c2822c2a98f25a7978519d2c8a0625
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-04-04 19:18:47 +0600

    question ans solved

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './Blogs.css'
 
 const Blogs = () => {
     return (
-        <div>
-            
+        <div className='question-ans'>
+        <b>Question1: what are semantic tag?</b> 
+        <p><b>Ans: </b> A semantic element clearly describes its meaning to both the browser and the developer.Generally users can not understand during visit the webpage properly without semantic tag.when programmer use semantic tag then easily understand the code.Each semantic tag is a meaningful word. semantic elements: form tag, table tag, and img tag- Clearly defines its content.It has greater accessibility. It offers a better user experience.This semantic HTML tags help the search engines and other user devices to determine the importance and context of web pages.</p>
+
+        <b>Question2: Difference between inline,block and inline-block element/</b>
+        <p><b>Ans: </b></p>
+
+
         </div>
     );
 };
